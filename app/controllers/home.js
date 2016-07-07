4c2deacfa3df777ce1286849b1781846dbc486da
Spyros Psychogios <spyros@enki.com>
2016-07-07 02:15:39 +0100

    just render

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,11 +2,13 @@ var router = require('express').Router();
 const request = require('superagent');
 
 var home = function (req, res) {
-  var data = {
-  };
-  req.session.error = null;
-  req.session.success = null;
-  res.render('index.html', data);
+  // var data = {
+  //   err: req.session.error,
+  //   success: req.session.success,
+  // };
+  // req.session.error = null;
+  // req.session.success = null;
+  res.render('index.html');
 };
 
 router.get('/', home);
