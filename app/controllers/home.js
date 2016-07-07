5e47187e4780de767c5d8d809b5cde15cf357ac6
Spyros Psychogios <spyros@enki.com>
2016-07-07 02:14:13 +0100

    no data

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,8 +3,6 @@ const request = require('superagent');
 
 var home = function (req, res) {
   var data = {
-    err: req.session.error,
-    success: req.session.success,
   };
   req.session.error = null;
   req.session.success = null;
