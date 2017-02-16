a1a3bb6eacf71e8a2c2d2691ac50e8735008afb8
Spyros Psychogios <spyros@enki.com>
2017-02-16 22:20:44 +0000

    /api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,7 +12,7 @@
   // console.log(form)
   form.addEventListener("submit", function(e) {
     e.preventDefault();
-    xhr.open("POST", url + "/register-email", true);
+    xhr.open("POST", url + "/api/register-email", true);
     xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhr.onload = function() {
       // if (window.ga) {
