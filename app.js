92861fc10f0189310f7dc7f60c29648fbe055525
Spyros Psychogios <spyros@enki.com>
2017-02-16 20:18:41 +0000

    try enki config

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /*jshint node: true*/
 var express = require('express');
-// var session = require('express-session');
+var session = require('express-session');
 var bodyParser = require('body-parser');
 var compress = require('compression');
 
@@ -10,7 +10,7 @@ var router = require(__dirname + '/app/controllers');
 app.engine('html', require('ejs').renderFile);
 app.set('views', __dirname + '/app');
 app.set('view engine', 'ejs');
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3001));
 
 var options = {
   dotfiles: 'ignore',
@@ -21,23 +21,20 @@ var options = {
 };
 
 app.use(compress());
-
-app.use(express.static(__dirname + '/app', options));
+app.use(express.static(__dirname + '/app/public', options));
 //app.use(express.cookieParser());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-// app.use(session({ secret: 'keyboard cat' }));
+app.use(session({ secret: 'keyboard cat' }));
 app.use(router.router);
-
-console.log('I AM RUNNING');
 
 var server = app.listen(app.get('port'), function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('GoQuoteMe test app listening at http://%s:%s', host, port);
+  console.log('FilmBeat test app listening at http://%s:%s', host, port);
 
 });
--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,11 +1,11 @@
 var router = require('express').Router();
-const request = require('superagent');
 
 var home = function (req, res) {
   // var data = {
   //   err: req.session.error,
   //   success: req.session.success,
   // };
+
   // req.session.error = null;
   // req.session.success = null;
   res.render('index.html');
